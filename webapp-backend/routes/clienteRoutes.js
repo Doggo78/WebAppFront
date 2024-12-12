bc079497e878673719bab33d7c3eea08b8fe35ef
Mv1cc <mario.villena16@gmail.com>
2024-12-11 22:48:27 -0300

    quito back

--- webapp-backend/routes/clienteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createCliente } = require('../controllers/clienteController');
-const authenticateToken = require('../middlewares/authenticateToken');
-
-const router = express.Router();
-
-router.post('/', authenticateToken, createCliente);
-
-module.exports = router;
